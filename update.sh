5f1be07a9311e37e3a1048b251b8bde2e09f305f
StevenWojcik <s.wojcik419@gmail.com>
2022-12-25 16:28:15 -0500

    added XRDP and qemu-guest-agent

--- update.sh
+++ update.sh
@@ -1,10 +1,28 @@
 #! /bin/bash
 
+# Run Update and Upgrade commands on system
 sudo apt update
 sudo apt upgrade
+
+# Remote Desktop
+sudo apt install xrdp
+    # add to startup
+    systemctl enable xrdp
+
+
+
+
+
+# System Status and Monitoring 
+sydo apt install qemu-guest-agent
 sudo apt install neofetch
 sudo apt install nano
 sudo apt install htop
+
+
+# install "brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-neofetch
+
+# add to systemctl services
+systemctl enable qemu-guest-agent
