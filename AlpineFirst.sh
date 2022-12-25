048f0a01a9eed2bee237ae2a6ca787d15eb2000d
StevenWojcik <s.wojcik419@gmail.com>
2022-12-25 17:43:43 -0500

    added Docker and Portainer install

--- AlpineFirst.sh
+++ AlpineFirst.sh
@@ -23,4 +23,6 @@ apk add docker
 rc-service docker enable
 rc-update add docker
 
-
+#Install Portainer 
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
