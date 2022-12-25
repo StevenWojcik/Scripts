62fa3be152967f1938312540a7bdbec6fcb9bca6
StevenWojcik <s.wojcik419@gmail.com>
2022-12-25 16:19:53 -0500

    updated slightly

--- update.sh
+++ update.sh
@@ -3,4 +3,8 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install neofetch
+sudo apt install nano
+sudo apt install htop
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 neofetch
