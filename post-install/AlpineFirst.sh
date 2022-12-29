a6e4c54e6d935e9aca71bbd7091da69c7b92131f
StevenWojcik <s.wojcik419@gmail.com>
2022-12-29 16:01:21 -0500

    h

--- post-install/AlpineFirst.sh
+++ post-install/AlpineFirst.sh
@@ -42,6 +42,10 @@
   git config --global user.name "StevenWojcik"
   git config --global user.email "S.wojcik419@gmail.com"
 
+#-Install Portainer Agent
+docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest
+
+
 #-Install Portainer 
-  docker volume create portainer_data
-  docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+#  docker volume create portainer_data
+#  docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
